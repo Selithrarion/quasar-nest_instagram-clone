a8582b5fa320daf911208cee3897b5ab84a3c400
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-03 15:53:33 +0500

    f-feed: fix page

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,6 +1,5 @@
 <template>
-  <q-page>
-  </q-page>
+  <q-page> </q-page>
 </template>
 
 <script lang="ts">
@@ -12,22 +11,13 @@ import useDialog from 'src/composables/common/useDialog';
 import useLoading from 'src/composables/common/useLoading';
 
 import CommonPageTitle from 'components/common/CommonPageTitle.vue';
-import CommonDialogConfirmEmail from 'components/common/CommonDialogConfirmEmail.vue';
-import WorkProjectHeader from 'components/work/WorkProjectHeader.vue';
-import WorkProjectList from 'components/work/WorkProjectList.vue';
-import WorkProjectCard from 'components/work/WorkProjectCard.vue';
-
 import userRepository from 'src/repositories/userRepository';
 
 export default defineComponent({
-  name: 'WorkIndex',
+  name: 'FeedIndex',
 
   components: {
     CommonPageTitle,
-    CommonDialogConfirmEmail,
-    WorkProjectHeader,
-    WorkProjectList,
-    WorkProjectCard,
   },
 
   setup() {
@@ -38,40 +28,18 @@ export default defineComponent({
     const dialog = useDialog();
     const loading = useLoading();
 
-    const availableProjects = computed(() => store.state.project.projects);
-    async function openAllProjects() {
-      return await router.push({ path: '/projects', query: { instantCreate: 1 } });
-    }
+    const availablePosts = computed(() => store.state.post.posts);
 
     onBeforeMount(async () => {
-      if (!availableProjects.value) await store.dispatch('project/getAll');
-      await tryConfirmEmail();
+      if (!availablePosts.value) await store.dispatch('post/getAll');
     });
-
-    const isEmailConfirmSuccess = ref(false);
-    async function tryConfirmEmail() {
-      const isEmailConfirmed = store.state.user.currentUser?.isEmailConfirmed;
-      if (!route.query.token || isEmailConfirmed) return;
-
-      try {
-        dialog.open('confirmEmail');
-        dialog.startLoading();
-        await userRepository.confirmEmail(String(route.query.token));
-        isEmailConfirmSuccess.value = true;
-      } finally {
-        dialog.stopLoading();
-      }
-    }
 
     return {
       dialog,
       loading,
       t,
 
-      availableProjects,
-      openAllProjects,
-
-      isEmailConfirmSuccess,
+      availablePosts,
     };
   },
 });
