858472ce98b07d4c25546fe05453dac68928a466
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-03 15:51:35 +0500

    f-post: add repository

---
+++ frontend/src/repositories/postRepository.ts
@@ -0,0 +1,29 @@
+import { http } from 'boot/axios';
+import { PostDTO, PostModel } from 'src/models/post/post.model';
+import { ApiResponseModel } from 'src/models/common/apiResponse.model';
+
+export default {
+  async getAll({ page = 1, limit = 5 } = { page: 1, limit: 5 }): Promise<PostModel[]> {
+    const { data }: ApiResponseModel<PostModel[]> = await http.get('/posts', { params: { page, limit } });
+    return data;
+  },
+  async getByID(id: number): Promise<PostModel> {
+    const { data }: ApiResponseModel<PostModel> = await http.get(`/posts/${id}`);
+    return data;
+  },
+  async create(postData: PostDTO): Promise<PostModel> {
+    const { data }: ApiResponseModel<PostModel> = await http.post('/posts', postData);
+    return data;
+  },
+  async update(id: number, postData: Partial<PostDTO>): Promise<PostModel> {
+    const { data }: ApiResponseModel<PostModel> = await http.patch(`/posts/${id}`, postData);
+    return data;
+  },
+  async delete(id: number): Promise<void> {
+    return await http.delete(`/posts/${id}`);
+  },
+
+  async toggleLike(id: number): Promise<void> {
+    return await http.post(`/posts/favorite/${id}`);
+  },
+};
