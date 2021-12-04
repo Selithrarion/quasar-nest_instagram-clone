51d20de8453c7b054182c05f78a34efedb5e0a47
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-04 14:40:07 +0500

    b-post: add dtos

---
+++ backend/src/modules/posts/dto/index.ts
@@ -0,0 +1,19 @@
+import { IsBoolean, IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+
+export class CreatePostDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  tags: string[];
+
+  @IsBoolean()
+  isVideo: boolean;
+}
+
+export class UpdatePostDTO extends CreatePostDTO {}
