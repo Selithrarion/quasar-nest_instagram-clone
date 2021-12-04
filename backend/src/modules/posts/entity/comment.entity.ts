33c08a73dae85a6d0a3f21137fcb8d4a730de202
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-04 14:40:16 +0500

    b-post: add comment entity

---
+++ backend/src/modules/posts/entity/comment.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, RelationId } from 'typeorm';
+import { BaseEntity } from '../../../common/types/base.entity';
+import { UserEntity } from '../../user/entity/user.entity';
+import { PostEntity } from './post.entity';
+
+@Entity()
+export class CommentEntity extends BaseEntity {
+  @Column()
+  text: string;
+
+  @ManyToOne(() => PostEntity, (post) => post.comments, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'postID' })
+  post: PostEntity;
+
+  @ManyToOne(() => UserEntity, (user) => user.comments, {
+    eager: true,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'authorID' })
+  author: UserEntity;
+
+  @OneToMany(() => UserEntity, (user) => user.comments, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'issueID' })
+  likes: UserEntity[];
+
+  @RelationId('likes')
+  likesNumber: number;
+}
