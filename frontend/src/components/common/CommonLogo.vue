bcd49566891cccadd4eeb6013b72ae87ba8236e0
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-07 12:23:50 +0500

    f-common: add logo component

---
+++ frontend/src/components/common/CommonLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommonLogo',
+});
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  filter: sepia(1) contrast(0.1) saturate(60) hue-rotate(190deg) brightness(0.7);
+  margin-top: 6px;
+}
+</style>
