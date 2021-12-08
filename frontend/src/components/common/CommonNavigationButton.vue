2d33ed9dc84c42ae1184bac821a86b726b6cd7ac
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-08 19:05:56 +0500

    f-common: add navigation button

---
+++ frontend/src/components/common/CommonNavigationButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseButton
+    class="common-navigation-button"
+    size="10px"
+    :icon="type === 'previous' ? 'navigate_before' : 'navigate_next'"
+    :tooltip="type === 'previous' ? 'Show previous' : 'Show next'"
+    round
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommonNavigationButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'previous',
+      validator: (v: string) => {
+        return ['previous', 'next'].includes(v);
+      },
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.common-navigation-button {
+}
+</style>
