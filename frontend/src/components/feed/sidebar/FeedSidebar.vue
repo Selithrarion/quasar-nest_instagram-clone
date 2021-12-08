9a853ca24d929d3272a623c7fc661283c349de45
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-08 19:06:30 +0500

    f-feed: add sidebar, account, footer, recommendations

---
+++ frontend/src/components/feed/sidebar/FeedSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <aside class="feed-sidebar bg-grey-2">
+    <div class="content">123</div>
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedSidebar',
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.feed-sidebar {
+  position: sticky;
+  top: 32px;
+  flex-basis: 300px;
+  flex-grow: 1;
+  flex-shrink: 0;
+  .content {
+    max-height: calc(100vh - 32px * 2);
+  }
+}
+</style>
---
+++ frontend/src/components/feed/sidebar/FeedSidebarAccount.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedSidebarAccount',
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
