24efa0c70b272c70cc0056793d3b3f3ba6429edf
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-08 19:06:07 +0500

    f-feed: add post and post list

---
+++ frontend/src/components/feed/post/FeedPost.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="feed-post">{{ post }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { PostModel } from 'src/models/post/post.model';
+
+export default defineComponent({
+  name: 'FeedPost',
+
+  props: {
+    post: {
+      type: Object as PropType<PostModel>,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.feed-post {
+  width: 100%;
+}
+</style>
---
+++ frontend/src/components/feed/post/FeedPostList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="feed-post-list">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedPostList',
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.feed-post-list {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+}
+</style>
