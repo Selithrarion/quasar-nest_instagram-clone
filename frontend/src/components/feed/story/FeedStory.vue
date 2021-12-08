cc4701af2556cfa3821681f0d18ad9a145098c0b
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-08 19:06:42 +0500

    f-feed: add story and story list

---
+++ frontend/src/components/feed/story/FeedStory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="feed-story">{{ story }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedStory',
+
+  props: {
+    story: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.feed-story {
+  width: 80px;
+}
+</style>
---
+++ frontend/src/components/feed/story/FeedStoryList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="feed-story-list">
+    <CommonNavigationButton type="previous" />
+
+    <slot />
+
+    <CommonNavigationButton type="next" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CommonNavigationButton from 'components/common/CommonNavigationButton.vue';
+
+export default defineComponent({
+  name: 'FeedStoryList',
+
+  components: {
+    CommonNavigationButton,
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.feed-story-list {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px 0;
+  //width: 100%;
+  overflow: hidden;
+}
+</style>
