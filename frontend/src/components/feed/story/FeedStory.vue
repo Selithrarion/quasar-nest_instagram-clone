66ce15ef2e0f8556cf35e6498b44748c141f64af
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-10 14:42:28 +0500

    f-story: add item markup

--- frontend/src/components/feed/story/FeedStory.vue
+++ frontend/src/components/feed/story/FeedStory.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="feed-story">{{ story }}</div>
+  <div class="feed-story">
+    <BaseAvatar size="66px" src="https://cdn.quasar.dev/img/avatar.png" />
+    <div class="feed-story__name text-caption ellipsis">123123123123123123123</div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,6 +26,19 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .feed-story {
+  display: flex;
+  flex-flow: column;
+  gap: 4px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+
   width: 80px;
+  overflow: hidden;
+  min-width: 0;
+
+  &__name {
+    max-width: 80px;
+  }
 }
 </style>
