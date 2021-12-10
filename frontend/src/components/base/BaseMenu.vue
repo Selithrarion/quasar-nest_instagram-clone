f9e99bbef7821edf7a3f6600fb0b30d151f12d24
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-10 13:25:48 +0500

    f-base: add base menu

---
+++ frontend/src/components/base/BaseMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-menu style="min-width: 150px" :auto-close="autoClose">
+    <slot />
+  </q-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseMenu',
+
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
