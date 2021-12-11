96e5993b89c6ed3dbd37eb669e643ea7fd29eddb
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-11 18:49:22 +0500

    f-common: add messages icon

---
+++ frontend/src/components/common/CommonIconMessages.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseButton class="common-icon-messages" icon="send" size="16px" unelevated dense round />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommonIconMessages',
+});
+</script>
+
+<style lang="scss" scoped>
+.common-icon-messages {
+  transform: rotate(-30deg);
+  padding-bottom: 4px;
+  padding-left: 8px;
+}
+</style>
