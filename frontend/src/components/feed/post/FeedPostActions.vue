0747c90a18ee8c223328b1c939f5036010878dbd
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-11 18:49:44 +0500

    f-post: add actions markup

---
+++ frontend/src/components/feed/post/FeedPostActions.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex-center-between q-pr-sm q-py-sm" style="padding-left: 6px">
+    <div>
+      <BaseButton
+        size="16px"
+        :icon="isViewerLiked ? 'favorite' : 'favorite_border'"
+        :tooltip="isViewerLiked ? 'Remove like' : 'Like'"
+        unelevated
+        dense
+        round
+        @click="toggleLike"
+      />
+      <BaseButton icon="forum" size="16px" tooltip="Show comments" unelevated dense round @click="openPostDetail" />
+      <CommonIconMessages tooltip="Share" />
+    </div>
+
+    <BaseButton
+      size="16px"
+      :icon="isViewerBookmarked ? 'bookmark' : 'bookmark_border'"
+      :tooltip="isViewerBookmarked ? 'Remove from bookmarks' : 'Save to bookmarks'"
+      unelevated
+      dense
+      round
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CommonIconMessages from 'components/common/CommonIconMessages.vue';
+
+export default defineComponent({
+  name: 'FeedPostActions',
+
+  components: {
+    CommonIconMessages,
+  },
+
+  props: {
+    isViewerLiked: Boolean,
+    isViewerBookmarked: Boolean,
+  },
+
+  setup() {
+    function toggleLike() {
+      //
+    }
+    function openPostDetail() {
+      //
+    }
+
+    return {
+      toggleLike,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
