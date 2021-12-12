fae372e806dee62b29ea483d88ca120f122c920a
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-12 19:01:33 +0500

    f-post: add post comment component

---
+++ frontend/src/components/feed/post/FeedPostComment.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="row justify-between gap-2 no-wrap">
+    <div>
+      <b> 123123123123 </b>
+      <span>
+        flex-wrap flex-wrap fleex-wrap fleex-wrap fleex-wrap flex-wrap flex-wrap flex-wrap flex-wrap flex-wrap flex-wrap
+      </span>
+    </div>
+
+    <BaseButton
+      class="self-center"
+      size="8px"
+      :text-color="comment % 2 ? 'red' : ''"
+      :icon="comment % 2 ? 'favorite' : 'favorite_border'"
+      :tooltip="comment % 2 ? 'Remove like' : 'Like'"
+      unelevated
+      dense
+      round
+      @click="toggleLike"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { CommentModel } from 'src/models/post/comment.model';
+
+export default defineComponent({
+  name: 'FeedPostComment',
+
+  props: {
+    comment: {
+      type: Object as PropType<CommentModel>,
+      required: true,
+    },
+    minimized: Boolean,
+  },
+
+  setup() {
+    function toggleLike() {
+      //
+    }
+
+    return {
+      toggleLike,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
