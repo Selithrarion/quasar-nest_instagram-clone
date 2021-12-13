f1d72610f05971d06b43348e501692f27c3a1fe0
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-13 12:34:16 +0500

    f-feed: add sidebar footer component

--- frontend/src/components/feed/sidebar/FeedSidebarFooter.vue
+++ frontend/src/components/feed/sidebar/FeedSidebarFooter.vue
@@ -1,17 +1,61 @@
 <template>
-  <div></div>
+  <div class="feed-sidebar-footer column gap-2 text-caption">
+    <div class="row gap-1 wrap">
+      <BaseButton
+        v-for="link in links"
+        :key="link.label"
+        :label="link.label"
+        plain-style
+        @click="openExternalPage(link.url)"
+      />
+    </div>
+    <div class="opacity-60">© 2021 INSTAGRAM FROM META</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import openExternalPage from 'app/utils/openExternalPage';
 
 export default defineComponent({
   name: 'FeedSidebarFooter',
 
   setup() {
-    return {};
+    const links = [
+      {
+        label: 'About',
+        url: 'https://about.instagram.com/',
+      },
+      {
+        label: 'Help',
+        url: 'https://help.instagram.com/',
+      },
+      {
+        label: 'Press',
+        url: 'https://about.instagram.com/en_US/blog',
+      },
+      {
+        label: 'API',
+        url: 'https://developers.facebook.com/docs/instagram',
+      },
+      {
+        label: 'Jobs',
+        url: 'https://about.instagram.com/about-us/careers',
+      },
+    ];
+
+    return {
+      openExternalPage,
+      links,
+    };
   },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.feed-sidebar-footer {
+  * {
+    color: $blue-grey-5 !important;
+  }
+}
+</style>
