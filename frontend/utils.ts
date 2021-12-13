add6e8aee3c041cb14015fa052aa744d054e38fd
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-13 12:34:01 +0500

    f-global: add openExternalPage util

--- frontend/utils.ts
+++
@@ -1,7 +0,0 @@
-export async function mockAsyncTimeout() {
-  return await new Promise((res) =>
-    setTimeout(() => {
-      res({});
-    }, 2000)
-  );
-}
---
+++ frontend/utils/openExternalPage.ts
@@ -0,0 +1,3 @@
+export default function openExternalPage(url: string) {
+  window.open(url, '_blank');
+}
