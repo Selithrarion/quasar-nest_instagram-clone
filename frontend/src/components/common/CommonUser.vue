9a2875262f1774cdc5f3634299b8b7e3bc250453
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-14 12:39:19 +0500

    f-common: add common user component

---
+++ frontend/src/components/common/CommonUser.vue
@@ -0,0 +1,50 @@
+<template>
+  <BaseItem>
+    <q-item-section side>
+      <BaseAvatar size="56px" :src="avatar" :item-name="username" :item-color="color" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ username }}</q-item-label>
+      <q-item-label caption>
+        <slot name="name" :user-name="name">
+          {{ name }}
+        </slot>
+      </q-item-label>
+    </q-item-section>
+
+    <slot name="append" />
+  </BaseItem>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommonUser',
+
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+
+    avatar: {
+      type: String,
+      default: null,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
