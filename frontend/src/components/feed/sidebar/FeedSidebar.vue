c0f86512ec41b07ac68db9186560a1e767a9a55b
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-14 13:01:22 +0500

    f-feed: add sidebar markup

--- frontend/src/components/feed/sidebar/FeedSidebar.vue
+++ frontend/src/components/feed/sidebar/FeedSidebar.vue
@@ -1,17 +1,53 @@
 <template>
-  <aside class="feed-sidebar bg-grey-2">
-    <div class="content">123</div>
+  <aside class="feed-sidebar">
+    <div class="column gap-2">
+      <CommonUser
+        class="q-px-xs"
+        :avatar="currentUser?.avatar?.url"
+        :color="currentUser.color"
+        :username="currentUser.username"
+        :name="currentUser.name"
+        @click="openCurrentUserProfile"
+      />
+
+      <FeedSidebarRecommendations />
+
+      <FeedSidebarFooter />
+    </div>
   </aside>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'src/store';
+import { useRouter } from 'vue-router';
+
+import CommonUser from 'components/common/CommonUser.vue';
+import FeedSidebarRecommendations from 'components/feed/sidebar/FeedSidebarRecommendations.vue';
+import FeedSidebarFooter from 'components/feed/sidebar/FeedSidebarFooter.vue';
 
 export default defineComponent({
   name: 'FeedSidebar',
 
+  components: {
+    CommonUser,
+    FeedSidebarRecommendations,
+    FeedSidebarFooter,
+  },
+
   setup() {
-    return {};
+    const store = useStore();
+    const router = useRouter();
+
+    const currentUser = computed(() => store.state.user.currentUser);
+    async function openCurrentUserProfile() {
+      if (currentUser.value) await router.push(`/${currentUser.value.username}`);
+    }
+
+    return {
+      currentUser,
+      openCurrentUserProfile,
+    };
   },
 });
 </script>
@@ -19,12 +55,10 @@ export default defineComponent({
 <style lang="scss" scoped>
 .feed-sidebar {
   position: sticky;
-  top: 32px;
-  flex-basis: 300px;
+  top: 98px;
   flex-grow: 1;
   flex-shrink: 0;
-  .content {
-    max-height: calc(100vh - 32px * 2);
-  }
+  max-height: calc(100vh - 64px * 2);
+  max-width: 270px;
 }
 </style>
