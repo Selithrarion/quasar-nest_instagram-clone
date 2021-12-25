9f44fb9269f0e3bb737e764d89971133c28c2338
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-25 17:15:41 +0500

    f-post: fix mutations

--- frontend/src/store/post/mutations.ts
+++ frontend/src/store/post/mutations.ts
@@ -4,21 +4,21 @@ import { PostModel } from 'src/models/post/post.model';
 import { PaginationData } from 'src/models/common/pagination.model';
 
 const mutation: MutationTree<PostStateInterface> = {
-  SET_POSTS(state, projects: PaginationData<PostModel[]>) {
-    state.posts = projects.items;
-    state.postsMeta = projects.meta;
+  SET_POSTS(state, posts: PaginationData<PostModel[]>) {
+    state.posts = posts.items;
+    state.postsMeta = posts.meta;
   },
-  UPDATE_POST(state: PostStateInterface, post: PostModel) {
+  UPDATE_POST(state, post: PostModel) {
     if (!state.posts) return;
     const index = state.posts.findIndex((d) => d.id === post.id);
     state.posts[index] = post;
   },
-  DELETE_POST(state: PostStateInterface, id: number) {
+  DELETE_POST(state, id: number) {
     if (!state.posts) return;
     const index = state.posts.findIndex((p) => p.id === id);
     state.posts.splice(index, 1);
   },
-  TOGGLE_FAVORITE(state: PostStateInterface, id: number) {
+  TOGGLE_FAVORITE(state, id: number) {
     const post = state.posts?.find((p) => p.id === id);
     if (post) post.isViewerLiked = !post.isViewerLiked;
   },
