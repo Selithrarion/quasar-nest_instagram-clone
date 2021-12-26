3f7c01d0ede4dadcfc5dcfb45e4afa58bd67f66d
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-26 18:34:49 +0500

    f-feed: add story skeleton

---
+++ frontend/src/components/feed/story/FeedStorySkeleton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex gap-1">
+    <q-skeleton class="flex-shrink-0" type="QAvatar" size="66px" />
+    <q-skeleton type="text" width="100%" class="text-caption" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedStorySkeleton',
+});
+</script>
