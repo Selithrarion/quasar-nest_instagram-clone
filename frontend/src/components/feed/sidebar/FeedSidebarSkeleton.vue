3061ee7d132b6743e1c4fd7e615f4b2c09ab0134
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-27 11:07:19 +0500

    f-feed: add sidebar skeleton

---
+++ frontend/src/components/feed/sidebar/FeedSidebarSkeleton.vue
@@ -0,0 +1,27 @@
+<template>
+  <aside class="column gap-2 full-width">
+    <div class="row items-center gap-2">
+      <q-skeleton type="QAvatar" size="56px" />
+      <q-skeleton type="text" width="50%" height="20px" />
+    </div>
+
+    <div class="column gap-1">
+      <div v-for="item in 5" :key="item" class="row items-center gap-2" style="height: 48px">
+        <q-skeleton type="QAvatar" size="32px" />
+        <q-skeleton type="text" width="50%" height="20px" />
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedSidebarSkeleton',
+
+  setup() {
+    return {};
+  },
+});
+</script>
