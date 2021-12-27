29e539d4e5b0ad3c7898f1b2c5857da9f5795190
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-27 11:09:14 +0500

    f-feed: add feed skeleton

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,16 +1,32 @@
 <template>
   <q-page class="row gap-4 q-px-md no-wrap">
-    <div class="feed">
-      <FeedStoryList>
-        <FeedStory v-for="story of 20" :key="story" :story="story" />
-      </FeedStoryList>
+    <template v-if="isLoadingState">
+      <div class="feed">
+        <FeedStoryList :show-navigation="false">
+          <FeedStorySkeleton v-for="story of 20" :key="story" />
+        </FeedStoryList>
 
-      <FeedPostList>
-        <FeedPost v-for="post of 30" :key="post" :post="post" />
-      </FeedPostList>
-    </div>
+        <FeedPostList>
+          <FeedPostSkeleton v-for="post of 2" :key="post" />
+        </FeedPostList>
+      </div>
 
-    <FeedSidebar />
+      <FeedSidebarSkeleton />
+    </template>
+
+    <template v-else>
+      <div class="feed">
+        <FeedStoryList>
+          <FeedStory v-for="story of 20" :key="story" :story="story" />
+        </FeedStoryList>
+
+        <FeedPostList>
+          <FeedPost v-for="post of 30" :key="post" :post="post" />
+        </FeedPostList>
+      </div>
+
+      <FeedSidebar />
+    </template>
   </q-page>
 </template>
 
@@ -24,9 +40,14 @@ import useLoading from 'src/composables/common/useLoading';
 
 import FeedStoryList from 'components/feed/story/FeedStoryList.vue';
 import FeedStory from 'components/feed/story/FeedStory.vue';
+import FeedStorySkeleton from 'components/feed/story/FeedStorySkeleton.vue';
+
 import FeedPostList from 'components/feed/post/FeedPostList.vue';
 import FeedPost from 'components/feed/post/FeedPost.vue';
+import FeedPostSkeleton from 'components/feed/post/FeedPostSkeleton.vue';
+
 import FeedSidebar from 'components/feed/sidebar/FeedSidebar.vue';
+import FeedSidebarSkeleton from 'components/feed/sidebar/FeedSidebarSkeleton.vue';
 
 export default defineComponent({
   name: 'FeedIndex',
@@ -34,9 +55,12 @@ export default defineComponent({
   components: {
     FeedStoryList,
     FeedStory,
+    FeedStorySkeleton,
     FeedPostList,
     FeedPost,
+    FeedPostSkeleton,
     FeedSidebar,
+    FeedSidebarSkeleton,
   },
 
   setup() {
@@ -48,6 +72,7 @@ export default defineComponent({
     const loading = useLoading();
 
     const availablePosts = computed(() => store.state.post.posts);
+    const isLoadingState = computed(() => store.state.app.isLoadingState);
 
     onBeforeMount(async () => {
       if (!availablePosts.value) await store.dispatch('post/getAll');
@@ -59,6 +84,7 @@ export default defineComponent({
       t,
 
       availablePosts,
+      isLoadingState,
     };
   },
 });
