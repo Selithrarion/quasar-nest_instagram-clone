2251580692a69aa6cc3e280b962948544088b46c
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-30 20:21:32 +0500

    f-post: add comment input focusInput method

--- frontend/src/components/feed/post/FeedPostCommentInput.vue
+++ frontend/src/components/feed/post/FeedPostCommentInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row gap-2 q-pl-md q-pr-sm q-pb-sm">
-    <q-input v-model="text" class="flex-grow-1" placeholder="Add a comment..." filled autogrow dense>
+    <q-input ref="input" v-model="text" class="flex-grow-1" placeholder="Add a comment..." filled autogrow dense>
       <template #prepend>
         <CommonEmojiPicker v-model="text" />
       </template>
@@ -24,15 +24,22 @@ export default defineComponent({
   },
 
   setup() {
+    const input = ref<HTMLInputElement>();
     const text = ref('');
     function addComment() {
       //
       text.value = '';
     }
 
+    function focusInput() {
+      input.value?.focus();
+    }
+
     return {
+      input,
       text,
       addComment,
+      focusInput,
     };
   },
 });
