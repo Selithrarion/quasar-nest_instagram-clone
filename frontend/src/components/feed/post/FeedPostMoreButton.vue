22a6916f2241536dad650651b62e67b136fd413c
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-01-04 09:30:02 +0500

    f-post: add more button component

---
+++ frontend/src/components/feed/post/FeedPostMoreButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseButtonMore>
+    <BaseMenu>
+      <BaseItem label="Report" danger />
+      <BaseItem :label="isViewerFollowed ? 'Unfollow' : 'Follow'" :danger="isViewerFollowed" />
+      <BaseItem label="Go to post" />
+      <BaseItem label="Share to..." />
+      <BaseItem label="Copy link" />
+      <BaseItem label="Embed" />
+      <BaseItem label="Cancel" />
+    </BaseMenu>
+  </BaseButtonMore>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeedPostMoreButton',
+
+  props: {
+    isViewerFollowed: Boolean,
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
