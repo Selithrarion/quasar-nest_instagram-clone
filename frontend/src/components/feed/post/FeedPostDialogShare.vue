2edccd2ac3d817d3cd29789d2cf55019705b0edc
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-01-06 07:53:11 +0500

    f-post: add share dialog content

--- frontend/src/components/feed/post/FeedPostDialogShare.vue
+++ frontend/src/components/feed/post/FeedPostDialogShare.vue
@@ -1,15 +1,27 @@
 <template>
-  <BaseDialog large v-bind="$attrs" @close="$emit('close')">
-    <template #content> share me!!! </template>
+  <BaseDialog small @close="$emit('close')" @click="$emit('close')">
+    <template #content>
+      <BaseItem label="Share to direct" disabled @click="$emit('close')" />
+      <BaseItem label="Share to Facebook" disabled @click="$emit('close')" />
+      <BaseItem label="Share to Twitter" disabled @click="$emit('close')" />
+      <CommonClipboard copy-url @click="$emit('close')">
+        <BaseItem label="Copy link" />
+      </CommonClipboard>
+    </template>
   </BaseDialog>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
+import CommonClipboard from 'components/common/CommonClipboard.vue';
 import { PostModel } from 'src/models/post/post.model';
 
 export default defineComponent({
   name: 'FeedPostDialogShare',
+
+  components: {
+    CommonClipboard,
+  },
 
   props: {
     post: {
