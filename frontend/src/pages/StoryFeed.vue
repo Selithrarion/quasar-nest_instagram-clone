f692ce7bf1e3beaab021ebec8a99d5166eb50240
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-01-10 09:39:12 +0500

    f-story: add story feed html

---
+++ frontend/src/pages/StoryFeed.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="bg-grey-10">
+    <CommonLogo class="absolute-top-left q-pa-md" white />
+
+    <div class="flex-center window-height q-py-md">
+      <StoryItem />
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import CommonLogo from 'components/common/CommonLogo.vue';
+import StoryItem from 'components/story/StoryItem.vue';
+
+export default defineComponent({
+  name: 'StoryFeed',
+
+  components: {
+    CommonLogo,
+    StoryItem,
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
