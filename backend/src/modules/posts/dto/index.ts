d69ee8d1d92effb207b3e280f91e4275273f3845
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-01-14 18:26:02 +0500

    b-post: add create comment dto

--- backend/src/modules/posts/dto/index.ts
+++ backend/src/modules/posts/dto/index.ts
@@ -1,8 +1,15 @@
-import { IsBoolean, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreatePostDTO {
   @IsString()
   description: string;
+}
+export class CreateCommentDTO {
+  @IsString()
+  text: string;
+
+  @IsNumber()
+  postID: number;
 }
 
 export class UpdatePostDTO extends CreatePostDTO {}
