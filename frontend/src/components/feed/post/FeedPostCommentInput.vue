56b082f3f08a274dd3f4d2c3399028e651392ac9
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-01-25 11:15:11 +0500

    f-post: fix comment input placeholder to add a reply if reply

--- frontend/src/components/feed/post/FeedPostCommentInput.vue
+++ frontend/src/components/feed/post/FeedPostCommentInput.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="row gap-2 q-pl-md q-pr-sm q-pb-sm">
-    <q-input ref="input" v-model="text" class="flex-grow-1" placeholder="Add a comment..." filled autogrow dense>
+    <q-input
+      ref="input"
+      v-model="text"
+      class="flex-grow-1"
+      :placeholder="`Add a ${replyComment ? 'reply' : 'comment'}...`"
+      filled
+      autogrow
+      dense
+    >
       <template #prepend>
         <CommonEmojiPicker v-model="text" />
         <q-chip
