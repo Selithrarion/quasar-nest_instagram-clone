50fe5aa52df45fdb73d123ed7d91fe08119a3a7e
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-01-31 12:00:08 +0500

    f-profile: add profile post and post list components

---
+++ frontend/src/components/profile/ProfilePost.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="profile-post">
+    <q-img :src="post.fileURL" ratio="1" />
+    <div class="profile-post__content absolute-center flex-center gap-8 full-height full-width">
+      <div class="flex-center gap-2">
+        <q-icon name="favorite" size="22px" />
+        {{ post.likesUserIDs.length }}
+      </div>
+      <div class="flex-center gap-2">
+        <q-icon name="forum" size="22px" />
+        {{ post.commentIDs.length }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { PostModel } from 'src/models/feed/post.model';
+
+export default defineComponent({
+  name: 'ProfilePost',
+
+  props: {
+    post: {
+      type: Object as PropType<PostModel>,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.profile-post {
+  position: relative;
+  width: 30%;
+  cursor: pointer;
+  &:hover {
+    .profile-post__content {
+      opacity: 1;
+    }
+  }
+  &__content {
+    font-size: 16px;
+    font-weight: 700;
+    color: white;
+    opacity: 0;
+    background: rgba(25, 18, 78, 0.26);
+  }
+}
+</style>
---
+++ frontend/src/components/profile/ProfilePostList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row gap-8 wrap">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProfilePostList',
+});
+</script>
