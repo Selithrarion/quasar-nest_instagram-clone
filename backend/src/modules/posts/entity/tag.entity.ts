238210a39c0c85b16b93c7e225702f54ed086697
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-02-17 12:21:31 +0500

    b-post: add tag entity name col index

--- backend/src/modules/posts/entity/tag.entity.ts
+++ backend/src/modules/posts/entity/tag.entity.ts
@@ -1,10 +1,11 @@
-import { Column, Entity, ManyToMany } from 'typeorm';
+import { Column, Entity, Index, ManyToMany } from "typeorm";
 import { BaseEntity } from '../../../common/types/base.entity';
 import { PostEntity } from './post.entity';
 
 @Entity()
 export class TagEntity extends BaseEntity {
   @Column()
+  @Index()
   name: string;
 
   @ManyToMany(() => PostEntity, (p) => p.tags, {
