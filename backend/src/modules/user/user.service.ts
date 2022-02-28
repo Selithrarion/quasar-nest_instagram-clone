f043b2e3f353397f79de113ab0b57e35bec22866
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-02-28 18:17:24 +0500

    b-user: fix notifications missing post and file

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -197,6 +197,8 @@ export class UserService {
       .createQueryBuilder('user')
       .where('user.id = :id', { id })
       .leftJoinAndSelect('user.notifications', 'notifications')
+      .leftJoinAndSelect('notifications.post', 'post')
+      .leftJoinAndSelect('post.file', 'file')
       .leftJoinAndSelect('notifications.user', 'notificationsUser')
       .leftJoinAndSelect('notificationsUser.avatar', 'notificationsUserAvatar')
       .orderBy('notifications.createdAt', 'DESC')
