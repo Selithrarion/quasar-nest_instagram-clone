2dd6eca7d809bd7fef3ec42156837705d1327e14
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-03-01 20:02:34 +0500

    b-notification: add create dto

--- backend/src/modules/notifications/dto/index.ts
+++ backend/src/modules/notifications/dto/index.ts
@@ -1,5 +1,12 @@
 import { IsBoolean } from 'class-validator';
+import { NotificationTypes } from '../entity/notification.entity';
 
+export class CreateNotificationDTO {
+  type: NotificationTypes;
+  receiverUserID: number;
+  initiatorUserID: number;
+  postID?: number;
+}
 export class UpdateNotificationDTO {
   @IsBoolean()
   read: boolean;
