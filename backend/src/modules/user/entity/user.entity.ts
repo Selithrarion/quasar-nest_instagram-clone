60f25844a89c4ae9537815460b225bc6a52f1c42
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-03-10 14:47:44 +0500

    b-user: fix likedPost relation (how it worked before LOL)

--- backend/src/modules/user/entity/user.entity.ts
+++ backend/src/modules/user/entity/user.entity.ts
@@ -159,7 +159,7 @@ export class UserEntity extends BaseEntity {
   })
   postFeed: PostFeedEntity[];
 
-  @ManyToMany(() => PostLikeEntity, (pl) => pl.user, {
+  @OneToMany(() => PostLikeEntity, (pl) => pl.user, {
     cascade: true,
   })
   likedPosts: PostLikeEntity[];
